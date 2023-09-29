7221c61c38d0eb0a37406464816ab814c9fc4ad6
Abulqasim <abulqasim.nazarov@gmail.com>
2023-09-29 11:26:09 +0400

    Create Academy App

--- Dapper/OrmModuleProject/OrmModuleProject/MainWindow.xaml.cs
+++ Dapper/OrmModuleProject/OrmModuleProject/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using ControlzEx.Theming;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace OrmModuleProject
 {
@@ -21,10 +22,49 @@ namespace OrmModuleProject
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer timer1;
+        private DispatcherTimer timer2;
+        private double progressValue = 100;
         public MainWindow()
         {
             InitializeComponent();
+            timer1 = new DispatcherTimer();
+            timer1.Interval = TimeSpan.FromSeconds(5); 
+            timer1.Tick += Timer_Tick;
+            timer1.Start();
+
+
+            timer2 = new DispatcherTimer();
+            timer2.Interval = TimeSpan.FromSeconds(1);
+            timer2.Tick += Timer_Tick;
+            timer2.Start();
+            UpdateTime();
+        }
+
+
+        
+
+        private void UpdateTime()
+        {
             
+            DateTime currentTime = DateTime.Now;
+
+            
+            timeTextBlock.Text = currentTime.ToString("HH:mm");
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            UpdateTime();
+
+            progressValue -= 10;
+            this.ZaradkaBar.Value = progressValue;
+            
+
+
+            if (progressValue <= 0)
+            {
+                timer1.Stop();
+            }
         }
     }
 }
